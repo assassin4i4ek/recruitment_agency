e792d5158be57507bd543b89539bc6da86b0fda3
Admin-ПК\Admin <yaneoyeah@gmail.com>
2017-12-07 22:00:46 +0200

    db create and insert

---
+++ src/main/resources/db/tspp_create.sql
@@ -0,0 +1,66 @@
+CREATE SCHEMA IF NOT EXISTS recruitment_agency;
+
+USE recruitment_agency;
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT,
+	username VARCHAR(255) NOT NULL UNIQUE,
+	password VARCHAR(100) NOT NULL,
+    enabled TINYINT NOT NULL DEFAULT 1,
+    role ENUM('ROLE_AGENT', 'ROLE_CANDIDATE', 'ROLE_ENTERPRISE') NOT NULL,
+    PRIMARY KEY(id)
+);
+			
+CREATE TABLE IF NOT EXISTS agents_info (
+	user_id INT NOT NULL,
+    PRIMARY KEY (user_id),
+    CONSTRAINT FK_user_id FOREIGN KEY (user_id)
+        REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS candidates_info (
+    user_id INT NOT NULL,
+	email VARCHAR(320) NOT NULL,
+    name VARCHAR(256),
+    PRIMARY KEY (user_id),
+    CONSTRAINT FK_candidate_id_user_id FOREIGN KEY (user_id)
+        REFERENCES users (id)
+);
+   
+CREATE TABLE IF NOT EXISTS enterprises_info (
+    user_id INT NOT NULL,
+	email VARCHAR(320) NOT NULL,
+    name VARCHAR(256),
+    PRIMARY KEY (user_id),
+    CONSTRAINT FK_enterprise_id_user_id FOREIGN KEY (user_id)
+        REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS applications (
+							id INT NOT NULL AUTO_INCREMENT,
+							enterprise_id INT NOT NULL,
+                            agent_id INT NOT NULL,
+							registration_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+							profession VARCHAR(100) NOT NULL,
+							quantity INT(3) DEFAULT 1,
+							agent_note TEXT,
+                            agent_order INT, 
+							PRIMARY KEY(id),
+							CONSTRAINT FK_applications_enterprise_id FOREIGN KEY(enterprise_id) REFERENCES enterprises_info(user_id),
+                            CONSTRAINT FK_applications_agent_id FOREIGN KEY(agent_id) REFERENCES agents_info(user_id));
+                            
+CREATE TABLE IF NOT EXISTS applicants_for_applications (
+										  id INT NOT NULL AUTO_INCREMENT,
+										  application_id INT NOT NULL,
+                                          candidate_id INT NOT NULL,
+                                          stage ENUM('INTERNAL_INVITATION',
+													 'INTERNAL_DENIED',
+                                                     'INTERNAL_PASSED',
+                                                     'EXTERNAL_INVITED',
+													 'EXTERNAL_DENIED',
+                                                     'EXTERNAL_PASSED',
+													 'GOT_JOB') DEFAULT 'INTERNAL_INVITATION',
+                                          applicant_order INT,           
+                                          PRIMARY KEY(id),
+                                          CONSTRAINT FK_application_id FOREIGN KEY(application_id) REFERENCES applications(id),
+                                          CONSTRAINT FK_candidate_id FOREIGN KEY(candidate_id) REFERENCES candidates_info(user_id));
---
+++ src/main/resources/db/tspp_insert.sql
@@ -0,0 +1,41 @@
+USE recruitment_agency;
+
+INSERT INTO `recruitment_agency`.`users` (`username`, `password`, `role`) VALUES
+('a1', 'a1', 'ROLE_AGENT'), 
+('a2', 'a2', 'ROLE_AGENT'),
+('e1', 'e1', 'ROLE_ENTERPRISE'),
+('e2', 'e2', 'ROLE_ENTERPRISE'),
+('c1', 'c1', 'ROLE_CANDIDATE'),
+('c2', 'c2', 'ROLE_CANDIDATE');
+
+SELECT * FROM users;
+
+INSERT INTO `recruitment_agency`.`agents_info`(`user_id`) VALUES (1);
+INSERT INTO `recruitment_agency`.`agents_info`(`user_id`) VALUES (2);
+
+SELECT * FROM agents_info;
+
+INSERT INTO `recruitment_agency`.`enterprises_info`(`user_id`,`name`, `email`) VALUES 
+(3, 'Enterprise1', 'e1@gmail.com'), (4, 'Enterprise2', 'e2@gmail.com');
+
+SELECT * FROM enterprises_info;
+
+INSERT INTO `recruitment_agency`.`candidates_info`(`user_id`,`name`, `email`) VALUES
+(5, 'Candidate1', 'c1@gmail.com'), (6, 'Candidate2', 'c2@gmail.com');
+
+SELECT * FROM candidates_info;
+
+INSERT INTO `recruitment_agency`.`applications`(`enterprise_id`,`agent_id`,`profession`,`quantity`,`agent_note`, `agent_order`)
+VALUES (4,2,'first',2,'this is a first test note of second agent.',1);
+INSERT INTO `recruitment_agency`.`applications`(`enterprise_id`,`agent_id`,`profession`,`quantity`,`agent_note`, `agent_order`)
+VALUES (4,2,'second',2,'this is a second test note of second agent.',2);
+INSERT INTO `recruitment_agency`.`applications`(`enterprise_id`,`agent_id`,`profession`,`quantity`,`agent_note`, `agent_order`)
+VALUES (4,2,'third',2,'this is a third test note of second agent.',3);
+
+SELECT * FROM applications;
+
+
+INSERT INTO `recruitment_agency`.`applicants_for_applications` (`application_id`,`candidate_id`) 
+VALUES(2, 5), (2, 6), (3, 5), (3, 6);
+
+SELECT * FROM applicants_for_applications;
